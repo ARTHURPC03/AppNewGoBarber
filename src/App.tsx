71b904f37b40b82dedda9ec89db9a554b7639015
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-05-19 18:03:12 -0300

    padrões de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from 'react'
+import { View, Text } from 'react-native'
 
 const App: React.FC = () => {
-  return <View />;
-};
+  return (
+    <View>
+      <Text>Oi</Text>
+    </View>
+  )
+}
 
-export default App;
+export default App
