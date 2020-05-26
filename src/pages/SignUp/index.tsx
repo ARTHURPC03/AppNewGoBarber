7a1fc16e1a284fe9436705961e922041fd4999e6
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-05-26 05:01:19 -0300

    Cadastro na aplicação

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import { useNavigation } from '@react-navigation/native'
 import { Form } from '@unform/mobile'
 import { FormHandles } from '@unform/core'
 import * as Yup from 'yup'
+import api from '../../services/api'
 
 import getValidationErrors from '../../utils/getValidationErrors'
 
@@ -53,9 +54,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       })
 
-      // await api.post('/users', data)
+      await api.post('/users', data)
 
-      // history.push('/')
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação.',
+      )
+
+      navigation.goBack()
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err)
