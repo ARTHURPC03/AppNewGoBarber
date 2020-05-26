cda12aa5f64bbe5d24f8d31d12860d94eb522903
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-05-25 21:08:53 -0300

    Validação dos formulários

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,10 @@
 import React, {
   useEffect,
+  useCallback,
   useRef,
   useImperativeHandle,
   forwardRef,
+  useState,
 } from 'react'
 import { TextInputProps } from 'react-native'
 import { useField } from '@unform/core'
@@ -31,6 +33,19 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   const { registerField, defaultValue = '', fieldName, error } = useField(name)
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue })
 
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true)
+  }, [])
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false)
+
+    setIsFilled(!!inputValueRef.current.value)
+  }, [])
+
   useImperativeHandle(ref, () => ({
     focus() {
       inputElementRef.current.focus()
@@ -54,13 +69,19 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField])
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#232129'}
+      />
       <TextInput
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={value => {
           inputValueRef.current.value = value
         }}
