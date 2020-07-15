42e61aa26ae85a57c5b36e955221d1c327bc5568
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-07-15 07:46:03 -0300

    Criando páginas e rotas

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,6 +3,10 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import Dashboard from '../pages/Dashboard'
 
+import Profile from '../pages/Profile'
+import CreateAppointment from '../pages/CreateAppointment'
+import AppointmentCreated from '../pages/AppointmentCreated'
+
 const App = createStackNavigator()
 
 const AppRoutes: React.FC = () => (
@@ -13,6 +17,10 @@ const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 )
 
