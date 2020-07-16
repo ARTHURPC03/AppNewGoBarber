c2d73a441633c64ac6e3d6f02d942d2c5d4dec2d
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-07-16 06:18:02 -0300

    Alternando entre providers

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,29 +1,56 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 import { useRoute, useNavigation } from '@react-navigation/native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useAuth } from '../../hooks/auth'
+import api from '../../services/api'
 import {
   Container,
   Header,
   BackButton,
   HeaderTitle,
   UserAvatar,
+  ProvidersList,
+  ProvidersListContainer,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
 } from './styles'
 
 interface RouteParams {
   providerId: string
 }
 
+export interface Provider {
+  id: string
+  name: string
+  avatar_url: string
+}
+
 const CreateAppointment: React.FC = () => {
   const { user } = useAuth()
   const route = useRoute()
-  const { providerId } = route.params as RouteParams
+  const routeParams = route.params as RouteParams
   const { goBack } = useNavigation()
+
+  const [providers, setProviders] = useState<Provider[]>([])
+  const [selectedProvider, setselectedProvider] = useState(
+    routeParams.providerId,
+  )
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data)
+    })
+  }, [])
 
   const navigateBack = useCallback(() => {
     goBack()
   }, [goBack])
+
+  const handleSelectProvider = useCallback((providerId: string) => {
+    setselectedProvider(providerId)
+  }, [])
 
   return (
     <Container>
@@ -36,6 +63,26 @@ const CreateAppointment: React.FC = () => {
 
         <UserAvatar source={{ uri: user.avatar_url }} />
       </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer
+              onPress={() => handleSelectProvider(provider.id)}
+              selected={provider.id === selectedProvider}
+            >
+              <ProviderAvatar source={{ uri: provider.avatar_url }} />
+              <ProviderName selected={provider.id === selectedProvider}>
+                {provider.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
     </Container>
   )
 }
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components/native'
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { FlatList } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { Provider } from './index'
+
+interface ProviderContainerProps {
+  selected: boolean
+}
+interface ProviderNameProps {
+  selected: boolean
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -29,4 +39,34 @@ export const UserAvatar = styled.Image`
   height: 56px;
   border-radius: 28px;
   margin-left: auto;
+`
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
+`
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+  background: ${props => (props.selected ? '#ff9000' : '#3e3b47')};
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  margin-right: 16px;
+  border-radius: 10px;
+`
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+`
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  margin-left: 8px;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
 `
