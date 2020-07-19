4cc53fb5756254eca5abc5ecda498188584b8b7f
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-07-19 05:16:40 -0300

    Criando estrutura da tela de sucesso

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useCallback } from 'react'
 
-// import { Container } from './styles'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
+import { Container, Title, Description, OkButton, OkButtonText } from './styles'
 
 const AppointmentCreated: React.FC = () => {
-  return <View />
+  const { reset } = useNavigation()
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    })
+  }, [reset])
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Terça, dia 14 de março de 2020 às 12:00h</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  )
 }
 
 export default AppointmentCreated
---
+++ src/pages/AppointmentCreated/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 24px;
+`
+export const Title = styled.Text`
+  font-size: 32px;
+  color: #f4edef;
+  font-family: 'RobotoSlab-Medium';
+  text-align: center;
+`
+
+export const Description = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  color: #999591;
+  margin-top: 16px;
+`
+
+export const OkButton = styled(RectButton)`
+  background: #ff9000;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  margin-top: 24px;
+  padding: 12px 24px;
+`
+
+export const OkButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #312e38;
+  font-size: 18px;
+`
